2166a6fb94c07ffbbcd92ae8465d920c5978c558
Derek Barnes <derekgeoffreybarnes@gmail.com>
2015-10-29 23:58:50 -0700

    fix * typo

--- prompt.c
+++ prompt.c
@@ -6,7 +6,7 @@
 
 static char buffer[2048];
 
-char* readline(char *prompt) {
+char *readline(char *prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
   char *cpy = malloc(strlen(buffer)+1);
