fcbd3e1766cce61a0bc19c6551576e963ce34277
Derek Barnes <derekgeoffreybarnes@gmail.com>
2015-10-31 15:31:04 -0700

    add exponent operator

--- parsing.c
+++ parsing.c
@@ -28,6 +28,7 @@ long eval_op(long x, char *op, long y) {
   if (strcmp(op, "*") == 0) { return x * y; }
   if (strcmp(op, "/") == 0) { return x / y; }
   if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return pow(x, y); }
   return 0;
 }
 
@@ -59,13 +60,13 @@ int main(int argc, char **argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                    \
       number   : /-?[0-9]+/ ;                            \
-      operator : '+' | '-' | '*' | '/' | '%';            \
+      operator : '+' | '-' | '*' | '/' | '%' |'^';       \
       expr     : <number> | '(' <operator> <expr>+ ')' ; \
       byol     : /^/ <operator> <expr>+ /$/ ;            \
     ",
     Number, Operator, Expr, Byol);
 
-  puts("Byol Version 0.0.0.0.6");
+  puts("Byol Version 0.0.0.0.7");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
